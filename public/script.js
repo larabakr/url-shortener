7903e454823acda1143a5470e394f4129b2992f5
dogeduaa <quzabubaker@gmail.com>
2021-06-26 12:28:23 +0300

    enhanced the UI links appearance

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ fetch('/api/links')
     .then(res => {
         res.forEach(element => {
            links.innerHTML += ` 
-           <p class="link">URL: <a href="${element.originalLink}" target="_blank">${element.originalLink}</a>, shortened: <a href="${element.short}" target="_blank">${element.short}</a></p>
+           <p class="link">original link: <a href="${element.originalLink}" target="_blank">${element.originalLink.length > 20 ? element.originalLink.substring(0, 20) + "..." : element.originalLink}</a>, shortened link: <a href="${element.short}" target="_blank">${element.short}</a></p>
            `;
         });
     })
